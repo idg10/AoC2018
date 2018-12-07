d4925788b8c42f29e0080f6e59ebc65d9ea12857
Ian Griffiths <ian@interact-sw.co.uk>
2018-12-07 08:01:37 +0000

    Fix up some Roslynator warnings in day 3

--- Solutions/Day03/Program.cs
+++ Solutions/Day03/Program.cs
@@ -16,7 +16,7 @@ namespace Day03
 {
     internal static class Program
     {
-        static void Main()
+        private static void Main()
         {
             // Parse lines of this form:
             //  #1 @ 1,3: 4x4
@@ -27,7 +27,6 @@ namespace Day03
                 from s3 in spaces
                 from s in pInt32ByInt32
                 select (id: id, x: p.x, y: p.y, w: s.w, h: s.h);
-
 
             Func<string, (int id, int x, int y, int w, int h)> parseLine = LineProcessor(lineParser);
 
@@ -87,7 +86,7 @@ namespace Day03
         /// </summary>
         /// <typeparam name="T">The claim type.</typeparam>
         /// <returns>An empty dictionaray.</returns>
-        static IImmutableDictionary<(int x, int y), IImmutableSet<T>> GetEmptyState<T>() => ImmutableDictionary<(int x, int y), IImmutableSet<T>>.Empty;
+        private static IImmutableDictionary<(int x, int y), IImmutableSet<T>> GetEmptyState<T>() => ImmutableDictionary<(int x, int y), IImmutableSet<T>>.Empty;
 
         /// <summary>
         /// Adds a rectangle of the specified dimensions with the given claim.
@@ -96,7 +95,7 @@ namespace Day03
         /// <param name="state">The current claims.</param>
         /// <param name="rectangle">Rectangle claim and dimensions.</param>
         /// <returns>The modified state.</returns>
-        static IImmutableDictionary<(int x, int y), IImmutableSet<T>> AddRectangle<T>(
+        private static IImmutableDictionary<(int x, int y), IImmutableSet<T>> AddRectangle<T>(
             IImmutableDictionary<(int x, int y), IImmutableSet<T>> state,
             (T id, int x, int y, int w, int h) rectangle)
         {
@@ -114,7 +113,7 @@ namespace Day03
         /// <param name="w">Rectangle width</param>
         /// <param name="h">Rectangle height.</param>
         /// <returns>The modified state.</returns>
-        static IImmutableDictionary<(int x, int y), IImmutableSet<T>> AddRectangle<T>(
+        private static IImmutableDictionary<(int x, int y), IImmutableSet<T>> AddRectangle<T>(
             IImmutableDictionary<(int x, int y), IImmutableSet<T>> state,
             T claim,
             int x,
@@ -147,7 +146,7 @@ namespace Day03
         /// <typeparam name="T">Claim type.</typeparam>
         /// <param name="state">The claims.</param>
         /// <returns>The number of cells that have more than one claim.</returns>
-        static int CountOverlappingCells<T>(IImmutableDictionary<(int x, int y), IImmutableSet<T>> state) =>
+        private static int CountOverlappingCells<T>(IImmutableDictionary<(int x, int y), IImmutableSet<T>> state) =>
             state.Count(kv => kv.Value.Count > 1);
 
         /// <summary>
@@ -158,7 +157,7 @@ namespace Day03
         /// <returns>
         /// The width and height of the smallest rectangle which, if its top left corner is at (0,0)
         /// contains all claimed cells.</returns>
-        static (int w, int h) GetDimensions<T>(IImmutableDictionary<(int x, int y), IImmutableSet<T>> state)
+        private static (int w, int h) GetDimensions<T>(IImmutableDictionary<(int x, int y), IImmutableSet<T>> state)
         {
             return state.Aggregate((w: 0, h: 0), (size, kv) => (Math.Max(size.w, kv.Key.x), Math.Max(size.h, kv.Key.y)));
         }
@@ -167,7 +166,7 @@ namespace Day03
         /// Prints the claims on screen, or an X for any cells with multiple claims.
         /// </summary>
         /// <param name="state">The claims.</param>
-        static void PrintState(IImmutableDictionary<(int x, int y), IImmutableSet<int>> state)
+        private static void PrintState(IImmutableDictionary<(int x, int y), IImmutableSet<int>> state)
         {
             (int w, int h) = GetDimensions(state);
             for (int j = 0; j <= h; ++j)
