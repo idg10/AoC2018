fa50e4a68e863e5ba16b7f9b0e39db0c2b136864
Ian Griffiths <ian@interact-sw.co.uk>
2018-12-28 09:09:51 +0000

    Day 11 part 1

---
+++ Solutions/Day11/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Day11
+{
+    public static class Program
+    {
+        public const int GridWidth = 300;
+        public const int GridHeight = 300;
+
+        private static void Main()
+        {
+            (int x, int y) = SolvePart1(5093);
+            Console.WriteLine($"Part 1: {x},{y}");
+        }
+
+        public static (int x, int y) SolvePart1(int serialNumber)
+        {
+            sbyte[,] windowSummedGrid = GenerateSummedGrid(3, 3, serialNumber);
+            return FindMax(windowSummedGrid);
+        }
+
+        public static sbyte CalculatePower(int x, int y, int serialNumber)
+        {
+            int rackId = x + 10;
+            int powerLevel = rackId * y;
+            powerLevel += serialNumber;
+            powerLevel *= rackId;
+
+            int hundredths = (powerLevel / 100) % 10;
+
+            return (sbyte) (hundredths - 5);
+        }
+
+        public static sbyte[] GenerateSummedRow(int windowSize, int y, int serialNumber)
+        {
+            var result = new sbyte[GridWidth - windowSize + 1];
+            var window = new sbyte[windowSize];
+            sbyte total = 0;
+            for (int x = 0; x < GridWidth; ++x)
+            {
+                int windowPos = x % windowSize;
+                total -= window[windowPos];
+                sbyte value = CalculatePower(x, y, serialNumber);
+                window[windowPos] = value;
+                total += value;
+                int resultIndex = x - windowSize + 1;
+                if (resultIndex >= 0)
+                {
+                    result[resultIndex] = total;
+                }
+            }
+
+            return result;
+        }
+
+        public static sbyte[,] GenerateSummedGrid(int windowWidth, int windowHeight, int serialNumber)
+        {
+            int outputWidth = GridWidth - windowWidth + 1;
+            var result = new sbyte[outputWidth, GridHeight - windowHeight + 1];
+            var window = new sbyte[windowHeight][];
+            var totals = new sbyte[outputWidth];
+
+            for (int y = 0; y < GridHeight; ++y)
+            {
+                int windowPos = y % windowHeight;
+                sbyte[] oldRow = window[windowPos];
+                sbyte[] newRow = GenerateSummedRow(windowWidth, y, serialNumber);
+                for (int x = 0; x < outputWidth; ++x)
+                {
+                    totals[x] -= oldRow?[x] ?? 0;
+                    totals[x] += newRow[x];
+                    int resultY = y - windowHeight + 1;
+                    if (resultY >= 0)
+                    {
+                        result[x, resultY] = totals[x];
+                    }
+
+                    window[windowPos] = newRow;
+                }
+            }
+            return result;
+        }
+
+        public static (int x, int y) FindMax(sbyte[,] values)
+        {
+            int max = sbyte.MinValue;
+            int mx = 0;
+            int my = 0;
+            int w = values.GetLength(0);
+            int h = values.GetLength(1);
+            for (int x = 0; x < w; ++x)
+            {
+                for (int y = 0; y < h; ++y)
+                {
+                    sbyte v = values[x, y];
+                    if (v > max)
+                    {
+                        mx = x;
+                        my = y;
+                        max = v;
+                    }
+                }
+            }
+
+            return (mx, my);
+        }
+    }
+}
---
+++ Solutions/Tests/Day11Tests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day11;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day11Tests
+    {
+        [TestMethod]
+        public void PowerLevelExample1()
+        {
+            Assert.AreEqual(4, Program.CalculatePower(3, 5, 8));
+        }
+
+        [TestMethod]
+        public void PowerLevelExample2()
+        {
+            Assert.AreEqual(-5, Program.CalculatePower(122, 79, 57));
+        }
+
+        [TestMethod]
+        public void PowerLevelExample3()
+        {
+            Assert.AreEqual(0, Program.CalculatePower(217, 196, 39));
+        }
+
+        [TestMethod]
+        public void PowerLevelExample4()
+        {
+            Assert.AreEqual(4, Program.CalculatePower(101, 153, 71));
+        }
+
+        [TestMethod]
+        public void PowerGridExample1Sums()
+        {
+            // Verify first row of their example grid matches cell by cell.
+            Assert.AreEqual(-2, Program.CalculatePower(32, 44, 18));
+            Assert.AreEqual(-4, Program.CalculatePower(33, 44, 18));
+            Assert.AreEqual(4, Program.CalculatePower(34, 44, 18));
+            Assert.AreEqual(4, Program.CalculatePower(35, 44, 18));
+            Assert.AreEqual(4, Program.CalculatePower(36, 44, 18));
+
+            // Verify second row of their example grid matches cell by cell.
+            Assert.AreEqual(-4, Program.CalculatePower(32, 45, 18));
+            Assert.AreEqual(4, Program.CalculatePower(33, 45, 18));
+            Assert.AreEqual(4, Program.CalculatePower(34, 45, 18));
+            Assert.AreEqual(4, Program.CalculatePower(35, 45, 18));
+            Assert.AreEqual(-5, Program.CalculatePower(36, 45, 18));
+
+            // Verify that our windowed sums for the first row look correct.
+            // -2  -4   4   4   4
+            // ----------           : -2  + -4 + 4 = -2
+            //     ----------       : -4  +  4 + 4 =  4
+            //          ---------   :  4  +  4 + 4 = 12
+            sbyte[] row1 = Program.GenerateSummedRow(3, 44, 18);
+            Assert.AreEqual(-2, row1[32]);
+            Assert.AreEqual(4, row1[33]);
+            Assert.AreEqual(12, row1[34]);
+
+            // Verify that our 2D windowed sums over the 1st 3 rows look correct.
+            sbyte[,] grid = Program.GenerateSummedGrid(3, 3, 18);
+            Assert.AreEqual(12, grid[32, 44]);
+            Assert.AreEqual(26, grid[33, 44]);
+            Assert.AreEqual(18, grid[34, 44]);
+
+            // ...and finally their actual example
+            Assert.AreEqual(29, grid[33, 45]);
+        }
+
+        [TestMethod]
+        public void PowerGridExample2Sum()
+        {
+            Assert.AreEqual(30, Program.GenerateSummedGrid(3, 3, 42)[21, 61]);
+        }
+
+        [TestMethod]
+        public void Part1Example1()
+        {
+            Assert.AreEqual((x: 33, y: 45), Program.SolvePart1(18));
+        }
+
+        [TestMethod]
+        public void Part1Example2()
+        {
+            Assert.AreEqual((x: 21, y: 61), Program.SolvePart1(42));
+        }
+    }
+}
