2f65757833070b3d38da8fdc00462aafe530871f
Ian Griffiths <ian@interact-sw.co.uk>
2019-01-02 18:37:10 +0000

    Day 14 part 1

---
+++ Solutions/Day14/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Day14
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            string first30 = new string(GenerateScoreboard(30)
+                .Select(i => (char) ('0' + i))
+                .ToArray());
+            Console.WriteLine(first30);
+
+            Console.WriteLine("Part 1: " + SolvePart1(165061));
+        }
+
+        public static string SolvePart1(int count) => new string(
+            GenerateScoreboard(count + 10)
+            .Skip(count)
+            .Take(10)
+            .Select(i => (char) ('0' + i))
+            .ToArray());
+
+        public static IEnumerable<byte> GenerateScoreboard(int length)
+        {
+            // Sadly, a naive immutable implementation is unusably slow, so we're using a
+            // mutable array to avoid allocations.
+            var scoreboard = new byte[length];
+            scoreboard[0] = 3;
+            scoreboard[1] = 7;
+            int next = 2;
+            var positions = new[] { 0, 1 };
+            while (next < length)
+            {
+                (var newRecipes, var newPositions) = GetNewRecipes(scoreboard, next, positions);
+                int i = 0;
+                while (next < length && i < newRecipes.Count)
+                {
+                    scoreboard[next++] = newRecipes[i++];
+                }
+
+                positions = newPositions;
+            }
+            return scoreboard;
+        }
+
+        public static (IList<byte> newRecipeScores, int[] elfPositions) GetNewRecipes(
+            IReadOnlyList<byte> scoreboard,
+            int scoreboardSize,
+            int[] elfPositions)
+        {
+            long total = elfPositions.Sum(p => scoreboard[p]);
+            long tt = total;
+            int digit;
+            int digits = 1 + (int) Math.Log10(Math.Max(1, total));
+            byte[] newRecipeScores = DigitArrays[digits - 1];
+            for (digit = 0; digit == 0 || tt != 0; ++digit)
+            {
+                newRecipeScores[newRecipeScores.Length - 1 - digit] = (byte) (tt % 10);
+                tt /= 10;
+            }
+
+            // It's a bit messy that we edit the positions in place as well as returning
+            // those changed positions. That's because when I wrote this, I started out
+            // with immutable lists, but the allocation costs were intolerable, so I made
+            // modifications to remove as many allocations as possible. So we now just
+            // return the same array of positions that was passed in, modifying the
+            // positions in situ. By avoiding per-iteration allocations, we speed things
+            // up by orders of magnitude.
+            int newScoreboardSize = scoreboardSize + digit;
+            for (int i = 0; i < elfPositions.Length; ++i)
+            {
+                int p = elfPositions[i];
+                elfPositions[i] = (p + scoreboard[p] + 1) % newScoreboardSize;
+            }
+
+            return (newRecipeScores, elfPositions);
+        }
+
+        // Reusable arrays to enable GetNewRecipes to return results of variable length without
+        // having to allocate a new array every time.
+        private static readonly byte[][] DigitArrays = Enumerable.Range(1, 10).Select(i => new byte[i]).ToArray();
+    }
+}
---
+++ Solutions/Tests/Day14Tests.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day14;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class Day14Tests
+    {
+        [TestMethod]
+        public void GetNewRecipesExample1()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(0, 1, 3, 7);
+            Assert.AreEqual(2, recipes.Length);
+            Assert.AreEqual(1, recipes[0]);
+            Assert.AreEqual(0, recipes[1]);
+
+            Assert.AreEqual(0, positions[0]);
+            Assert.AreEqual(1, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample2()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(0, 1, 3, 7, 1, 0);
+            Assert.AreEqual(2, recipes.Length);
+            Assert.AreEqual(1, recipes[0]);
+            Assert.AreEqual(0, recipes[1]);
+
+            Assert.AreEqual(4, positions[0]);
+            Assert.AreEqual(3, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample3()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(4, 3, 3, 7, 1, 0, 1, 0);
+            Assert.AreEqual(1, recipes.Length);
+            Assert.AreEqual(1, recipes[0]);
+
+            Assert.AreEqual(6, positions[0]);
+            Assert.AreEqual(4, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample4()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(6, 4, 3, 7, 1, 0, 1, 0, 1);
+            Assert.AreEqual(1, recipes.Length);
+            Assert.AreEqual(2, recipes[0]);
+
+            Assert.AreEqual(0, positions[0]);
+            Assert.AreEqual(6, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample5()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(0, 6, 3, 7, 1, 0, 1, 0, 1, 2);
+            Assert.AreEqual(1, recipes.Length);
+            Assert.AreEqual(4, recipes[0]);
+
+            Assert.AreEqual(4, positions[0]);
+            Assert.AreEqual(8, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample6()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(4, 8, 3, 7, 1, 0, 1, 0, 1, 2, 4);
+            Assert.AreEqual(1, recipes.Length);
+            Assert.AreEqual(5, recipes[0]);
+
+            Assert.AreEqual(6, positions[0]);
+            Assert.AreEqual(3, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample7()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(6, 3, 3, 7, 1, 0, 1, 0, 1, 2, 4, 5);
+            Assert.AreEqual(1, recipes.Length);
+            Assert.AreEqual(1, recipes[0]);
+
+            Assert.AreEqual(8, positions[0]);
+            Assert.AreEqual(4, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample8()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(8, 4, 3, 7, 1, 0, 1, 0, 1, 2, 4, 5, 1);
+            Assert.AreEqual(1, recipes.Length);
+            Assert.AreEqual(5, recipes[0]);
+
+            Assert.AreEqual(1, positions[0]);
+            Assert.AreEqual(6, positions[1]);
+        }
+
+        [TestMethod]
+        public void GetNewRecipesExample11()
+        {
+            (byte[] recipes, int[] positions) = MakeRecipes(1, 13, 3, 7, 1, 0, 1, 0, 1, 2, 4, 5, 1, 5, 8, 9);
+            Assert.AreEqual(2, recipes.Length);
+            Assert.AreEqual(1, recipes[0]);
+            Assert.AreEqual(6, recipes[1]);
+
+            Assert.AreEqual(9, positions[0]);
+            Assert.AreEqual(7, positions[1]);
+        }
+
+        private static (byte[] recipes, int[] positions) MakeRecipes(
+            int p1,
+            int p2,
+            params byte[] existing)
+        {
+            (IList<byte> scoreBoard, IEnumerable<int> elfPositions) = Program.GetNewRecipes(
+                ImmutableList.Create<byte>(existing), existing.Length, new[] { p1, p2 });
+            return (scoreBoard.ToArray(), elfPositions.ToArray());
+        }
+
+        [TestMethod]
+        public void Part1Example1()
+        {
+            Assert.AreEqual("5158916779", Program.SolvePart1(9));
+        }
+
+        [TestMethod]
+        public void Part1Example2()
+        {
+            Assert.AreEqual("0124515891", Program.SolvePart1(5));
+        }
+
+        [TestMethod]
+        public void Part1Example3()
+        {
+            Assert.AreEqual("9251071085", Program.SolvePart1(18));
+        }
+
+        [TestMethod]
+        public void Part1Example4()
+        {
+            Assert.AreEqual("5941429882", Program.SolvePart1(2018));
+        }
+
+        [TestMethod]
+        public void Part1Idg10()
+        {
+            Assert.AreEqual("5992684592", Program.SolvePart1(165061));
+        }
+    }
+}
