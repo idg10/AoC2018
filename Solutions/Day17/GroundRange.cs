c86bee98cee7fb67dc9f6186c2e379f81631d0eb
Ian Griffiths <ian@interact-sw.co.uk>
2019-01-12 08:42:27 +0000

    Day17 parsing

---
+++ Solutions/Day17/GroundRange.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day17
+{
+    public struct GroundRange
+    {
+        public GroundRange(
+            bool isVertical,
+            int position,
+            int start,
+            int end)
+        {
+            IsVertical = isVertical;
+            Position = position;
+            Start = start;
+            End = end;
+        }
+
+        public bool IsVertical { get; }
+        public int Position { get; }
+        public int Start { get; }
+        public int End { get; }
+
+        public override string ToString() => $"{(IsVertical ? 'x' : 'y')}={Position}, {(IsVertical ? 'y' : 'x')}={Start}..{End}";
+    }
+}
---
+++ Solutions/Day17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+using Common;
+using LanguageExt;
+using LanguageExt.Parsec;
+
+using static Common.Parsers;
+using static LanguageExt.Parsec.Char;
+using static LanguageExt.Parsec.Prim;
+
+namespace Day17
+{
+    public static class Program
+    {
+        public static readonly Parser<GroundRange> pRange =
+            from posDimension in either(ch('x'), ch('y'))
+            let isVertical = posDimension == 'x'
+            from eq1 in ch('=')
+            from pos in pInt32
+            from c in str(", ")
+            from rangeDimension in ch(isVertical ? 'y' : 'x')
+            from eq2 in ch('=')
+            from range in Sep2By(pInt32, str(".."))
+            select new GroundRange(isVertical, pos, range.Item1, range.Item2);
+
+        private static void Main()
+        {
+            var ranges = InputReader.ParseLines(typeof(Program), pRange).ToArray();
+        }
+    }
+}
